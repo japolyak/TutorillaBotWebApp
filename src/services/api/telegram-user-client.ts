05381ec14155ef430778406d708d7bb21277122a
Artem Kryvolap <artem.kryvolap@codefusion.pl>
2023-12-24 13:45:39 +0100

    send initdata in headers

--- src/services/api/telegram-user-client.ts
+++ src/services/api/telegram-user-client.ts
@@ -3,6 +3,8 @@ import type { InitDataDto } from '@/services/api/api.models'
 
 export class TelegramUserClient{
     public static async validateInitData(payload: InitDataDto): Promise<any> {
-        const request = httpClient.get('user/', { json: payload });
+        const request = httpClient.get('user/', { headers: {
+            'Init-Data': payload.initData
+        } });
     }
 }
