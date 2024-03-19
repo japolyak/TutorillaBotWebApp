1286bc43a7ddbf51f922d85c81d6daac05262038
japolyak <artemkryvolap@gmail.com>
2024-03-20 00:29:30 +0100

    For prod

--- src/services/api/clients/private-course-client.ts
+++ src/services/api/clients/private-course-client.ts
@@ -6,7 +6,6 @@ export class PrivateCourseClient {
         const request = httpClient.post(`private-courses/${privateCourseId}/new-class/`, { json: payload });
     }
 
-
 	public static async test(): Promise<any> {
         const request = await httpClient.post(``, { json: { test: 'test' } });
     }
