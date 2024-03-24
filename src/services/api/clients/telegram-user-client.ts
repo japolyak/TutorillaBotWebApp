c3633c74afc4b4dd5e48035ccbfee3306b188cb0
japolyak <artemkryvolap@gmail.com>
2024-03-24 23:20:34 +0100

    Urls change

--- src/services/api/clients/telegram-user-client.ts
+++ src/services/api/clients/telegram-user-client.ts
@@ -3,7 +3,7 @@ import { httpClient } from '../http-client';
 export class TelegramUserClient{
     public static async validateInitData(initData: string): Promise<any> {
 		try {
-			const request = await httpClient.get('auth/me', {headers: {'Init-Data': initData}});
+			const request = await httpClient.get('auth/me/', {headers: {'Init-Data': initData}});
 		} catch (error) {
 			console.error(error);
 		}
